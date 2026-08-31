0bb247892ee8f893c3e2557e2e76a767289793df
cdeimer <cullendeimer@gmail.com>
2026-08-30 22:26:30 -0400

    add stuff to map

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      exclude: ['maplibre-gl']
+    }
   }
 });
